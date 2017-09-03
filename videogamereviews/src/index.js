b8a9d6764d70c4cb0f05c856608262b6da563051
Vidsneezes <moraleszez@gmail.com>
2017-09-03 14:24:16 -0600

    added basic api setup

--- videogamereviews/src/index.js
+++ videogamereviews/src/index.js
@@ -4,7 +4,5 @@ import './index.css';
 import Routes from './route.js';
 import registerServiceWorker from './registerServiceWorker';
 
-const mongoose = require('mongoose');
-
 ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
